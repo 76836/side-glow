41959fdf26ae1c596596f887ba02a2beff0be0eb
76836 <74463275+76836@users.noreply.github.com>
2025-10-02 18:34:17 -0600

    Add files via upload

---
+++ stereo-test/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'TestLR-v1Cache';
+const urlsToCache = [
+        './',
+        './index.html'
+];
+
+// Install event - caching files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event - cleaning up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serving cached content
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
